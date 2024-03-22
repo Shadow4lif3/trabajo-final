7842b3c6e4cdfa6f5e1d2aa6e5eff075d11930de
Shadow4lif3 <160282797+Shadow4lif3@users.noreply.github.com>
2024-03-22 17:47:05 -0500

    Add files via upload

--- trabajo-final-cesde/usos.js
+++ trabajo-final-cesde/usos.js
@@ -9,57 +9,61 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
     slides[slideIndex-1].style.display = "block";  
-    setTimeout(showSlides, 2000); // Cambiar imagen cada 2 segundos
+    setTimeout(showSlides, 2000); 
 }
 
-// Obtener referencia al botón de inicio de sesión
+
 var loginButton = document.getElementById("loginButton");
 
-// Obtener referencia al modal de inicio de sesión
+
 var loginModal = document.getElementById("loginModal");
 
-// Obtener referencia al elemento de cierre del modal
 var closeBtn = document.getElementsByClassName("close")[0];
 
-// Cuando se haga clic en el botón de inicio de sesión, mostrar el modal
 loginButton.onclick = function() {
     loginModal.style.display = "block";
 }
 
-// Cuando se haga clic en el botón de cierre, ocultar el modal
+
 closeBtn.onclick = function() {
     loginModal.style.display = "none";
 }
 
-// Cuando el usuario haga clic en cualquier parte fuera del modal, ocultar el modal
+
 window.onclick = function(event) {
     if (event.target == loginModal) {
         loginModal.style.display = "none";
     }
 }
 
-// Obtener referencia al botón de registro
+
 var registerButton = document.getElementById("registerButton");
 
-// Obtener referencia al modal de registro
+
 var registerModal = document.getElementById("registerModal");
 
-// Obtener referencia al elemento de cierre del modal
+
 var closeBtn = document.getElementsByClassName("close2")[0];
 
-// Cuando se haga clic en el botón de registro, mostrar el modal
+
 registerButton.onclick = function() {
     registerModal.style.display = "block";
 }
 
-// Cuando se haga clic en el botón de cierre, ocultar el modal
+
 closeBtn.onclick = function() {
     registerModal.style.display = "none";
 }
 
-// Cuando el usuario haga clic en cualquier parte fuera del modal, ocultar el modal
 window.onclick = function(event) {
     if (event.target == registerModal) {
         registerModal.style.display = "none";
     }
+}
+
+function volverAlInicio() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Desplazamiento suave
+    });
 }
